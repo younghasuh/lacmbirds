b38b35b04f9a08d857f2a7df4c0b06ec9bef4398
younghasuh <ys757@cornell.edu>
2024-02-01 21:58:05 -0800

    adding files for tutorial

---
+++ lacm_birds/collectionsdata.R
@@ -0,0 +1,218 @@
+# R packages
+library(shiny)
+library(leaflet)
+library(urbnmapr)
+library(here)
+library(usmap)
+library(leaflet)
+library(sf)
+
+here::i_am("collectionsdata.R")
+
+# load data
+data <- read.csv("data_2023.csv")
+autocomplist <- data$species
+specnat <- read.csv(here("specnat.csv"))
+sta <- read.csv(here("states.csv"))
+
+
+#####
+# set up data
+inputdat <- data5
+
+data <- inputdat %>% 
+  select(lacm, sex, specnat, date, species, lat, lng) %>% 
+  mutate(
+    catalog = lacm,
+    date = as.Date(date),
+    year = as.numeric(format(date, "%Y")),
+    month = as.numeric(format(date, "%m")))
+
+
+
+# transform date into an actual date category
+data$date <- as.Date(data$datecoll, format="%d %B %Y")
+
+data$year <- as.numeric(format(data$date, "%Y"))
+data$month <- as.numeric(format(data$date, "%m"))
+
+
+#########
+# app starts here
+ui <- fluidPage(
+  tags$head(
+    tags$style(HTML('* {font-family: "Arial"};'))),
+  
+  titlePanel("LACM Specimen trends and maps"),
+  
+  mainPanel(
+    tabsetPanel(
+      type = "tabs",
+      
+      # tab 1
+      tabPanel(
+        titlePanel("Species summary"),
+        
+        sidebarLayout(
+          sidebarPanel(
+            
+            selectizeInput(
+              inputId = 'sp',
+              label = 'Species',
+              choices = NULL,
+              selected = NULL,
+              multiple = FALSE, # allow for multiple inputs
+              options = NULL),
+            
+            fluidRow(column(12, h4("Specimen count by specimen type/nature"), tableOutput("specnat")))
+          ),
+          
+          mainPanel(
+            
+            fluidRow(column(12, h4("Specimen count by year"), plotOutput("trend"))),
+            
+            fluidRow(column(12, h4("Specimen count by month"), plotOutput("trend2"))),
+            
+            fluidRow(column(12, h4("Specimen count by state (US only)"), plotOutput("state"))),
+            
+            fluidRow(column(12, h4("Global specimen distribution"), leafletOutput(outputId = 'map'))),
+            
+            fluidRow(
+              column(2, tableOutput("countbyyear")),
+              column(3, tableOutput("summary")))
+          )
+        )),
+      
+      # tab 2
+      tabPanel(
+        titlePanel("LACM lookup"),
+        textInput("catalog", "LACM"),
+        fluidRow(column(12, tableOutput("catcount"))),
+        fluidRow(column(12, h4("Leaftlet map"), leafletOutput(outputId = 'catmap')))
+      )
+    )))
+
+
+
+
+server <- function(input, output, session) {
+  
+  updateSelectizeInput(session, "sp", choices = autocomplist, selected=character(0), server = TRUE)
+  
+  selected <- reactive(data %>% filter(species == input$sp))
+  
+  output$countbyyear <- renderTable(
+    selected() %>% count(year)
+  )
+  
+  output$summary <- renderTable(
+    selected() %>% count(year, specnat)
+  )
+  
+  output$specnat <- renderTable(
+    selected() %>% count(Description)
+  )
+  
+  
+  trend1 <- reactive({
+    selected() %>% 
+      filter(specnat == "SS" | specnat == "SN" | specnat == "SW")
+  })
+  
+  output$trend <- renderPlot({
+    trend1() %>% 
+      ggplot(aes(x = year, fill = specnat, color = specnat)) +
+      geom_bar(position = position_dodge(preserve = "single")) +
+      scale_x_continuous(breaks = seq(1880, 2020, 10)) +
+      xlim(1850, 2023) +
+      theme_classic() +
+      labs(fill = "Specimen type", color = "Specimen type", x = "Year", y = "Count")
+    
+  }, res = 96)
+  
+  # count by month and type
+  output$trend2 <- renderPlot({
+    trend1() %>% 
+      ggplot(aes(x = month, fill = specnat, color = specnat)) +
+      geom_bar(position = position_dodge(preserve = "single")) +
+      scale_x_continuous(breaks = seq(1, 12, 1), labels = c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")) +
+      theme_classic() +
+      labs(fill = "Specimen type", color = "Specimen type", x = "Month", y = "Count")
+    
+  }, res = 96)
+  
+
+  # map by state
+  spat_state1 <- reactive({
+    left_join(get_urbn_map(map = "states", sf = TRUE),
+              selected() %>% 
+                count(abv),
+              by = c("state_abbv" = "abv"))
+  })
+  
+  # app format  
+  output$state <- renderPlot({
+    spat_state1() %>% 
+      ggplot() +
+      geom_sf(spat_state1(),
+              mapping = aes(fill = n),
+              color = "#ffffff", size = 0.25) +
+      labs(fill = "Specimen count") +
+      scale_fill_viridis_c(option = "D")  
+  })
+  
+  
+  # leaflet map
+  map_df <- reactive({
+    selected() %>% 
+      filter(!is.na(lng) & !is.na(lat)) %>% 
+      st_as_sf(coords = c("lng", "lat"))
+  })
+  
+  output$map = renderLeaflet({
+    leaflet() %>%
+      addTiles() %>%
+      addCircleMarkers(data = map_df(), radius=1) 
+  })  
+  
+  
+  
+  ##### Tab 2: LACM lookup
+  selected2 <- reactive(data %>% filter(lacm == input$catalog)) 
+  
+  output$catcount <- renderTable(
+    selected2() %>% 
+      mutate(
+        LACM = lacm,
+        LAF = laf,
+        Family = family,
+        Species = species,
+        Subspecies = spp,
+        Sex = sex,
+        Date = datecoll,
+        Locality = locality
+      ) %>% 
+      select(LACM, LAF, Family, Species, Subspecies, Sex, Date, Description, Locality)
+  )
+  
+  
+  
+  # using leaflet
+  catmap_df <- reactive({
+    selected2() %>% 
+      filter(!is.na(lng) & !is.na(lat)) %>% 
+      st_as_sf(coords = c("lng", "lat"))
+  })
+  
+  output$catmap = renderLeaflet({
+    leaflet() %>%
+      addTiles() %>%
+      addCircleMarkers(data = catmap_df(), radius=1)
+    
+  })
+  
+}
+
+
+# Run the application 
+shinyApp(ui = ui, server = server)
